82fe8e14e3eea0b01e3dd253834c4d5efedb43d1
Milza <marcomilza@gmail.com>
2025-03-27 09:33:38 +0100

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,29 @@
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./index.js", // Punto di ingresso
+  entry: "./react-app/src/main.tsx", // Modifica qui per indicare il tuo main.tsx
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"], // Permette di risolvere file .ts, .tsx, .js, .jsx
+  },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.tsx?$/, // Gestisce file TypeScript (.ts e .tsx)
+        use: "ts-loader",
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+      },
+      {
+        test: /\.scss$/, // Gestisce file SASS/SCSS
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "parent_app",
-      remotes: {
-        "react-app": "react_app@http://localhost:3001/remoteEntry.js",
-      },
-      shared: ["react", "react-dom"],
-    }),
-  ],
   devServer: {
-    static: path.join(__dirname, "public"), // Modifica 'contentBase' a 'static'
+    static: path.join(__dirname, "public"),
     port: 3000,
-    hot: true,
   },
 };
